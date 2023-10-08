0575e1b60c21e2a7ca4a23cc0b67e943fc9f0c12
wisamtaher <wisam.radwan@gmail.com>
2023-10-08 05:25:56 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - enty point
+ * Description: Print all the letters except q and e
+ * Return: zero
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
