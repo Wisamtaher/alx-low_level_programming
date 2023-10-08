6b666470ea6179cb55e7648994ac1ee09da07374
wisamtaher <wisam.radwan@gmail.com>
2023-10-08 06:19:37 -0700

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: single digit numbers of base 10 starting from 0
+ * Return: zer0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
