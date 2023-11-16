e1c577639add423ee2985b52b104f3dace456d8f
Wisamtaher <wisam.radwan@gmail.com>
2023-11-16 13:07:32 -0800

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+void ComefirstbyCTC(void)__attribute__((constructor));
+
+/**
+ * ComeFirst - Function
+ */
+
+void ComeFirstbyCTC(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
